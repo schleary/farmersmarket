b0ca82f7e30d384c4eee22b64c46512602da3718
Holly Leary <schleary@gmail.com>
2014-10-08 15:11:10 -0700

    fill out index controller

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,6 +1,20 @@
 class IndexController < ApplicationController
 
   def index
+    @vendors = Vendors.all
+  end
+
+  def new
+    @vendor = Vendor.new
+  end
+
+  def create
+    @vendor = Vendor.new(params.require(:vendor).permit(:name, :employees, :market_id))
+    if @vendor.save
+      redirect_to root_path
+    else
+
+    end
   end
 
 end
